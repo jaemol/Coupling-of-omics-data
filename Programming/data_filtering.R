2130ae89da4b7b532a257bd7828c62fd9d912e6c
Troublemolle <jmoller72@gmail.com>
2022-02-10 10:03:05 +0100

    Update data_filtering.R

--- Programming/data_filtering.R
+++ Programming/data_filtering.R
@@ -8,19 +8,51 @@
 dataLength = len_16s + len_qpcr
 inData = data
 
+
+## We start by running through and filtering out zero-columns
+throwAway <- c()
+# disregarding the first two columns - ID and OUA
+for (i in 3:length(inData[1,])) {
+  # if the variance is equal to zero, then its a zero-column
+  if (var(inData[,i]) == 0 | is.na(var(inData[,i]))) {
+    throwAway = rbind(throwAway, i)
+  }
+}
+
+# dropping columns by index
+inData = inData[, -throwAway] # losing 12230 attributes
+
+# checking how many of the respective omics attributes were dropped
+print("Number of metataxonomic attributes dropped") 
+length(throwAway[throwAway <= len_16s]) # 12199
+
+print("Number of genomic attributes dropped")
+length(throwAway[throwAway > len_16s])  # 31
+
+## next, we filtrate based on the worth of the attributes, by finding a baseline
 # coefficient for checking the worth of the attributes: 
-## variance / mean
-sumCoef = 0
+## standard variation / mean
+listCoef <- c()
 # for loop running through the given data set, finding the mean coefficient values for particular set
-for (i in 3:length(inData[,1])){
-  tempCoef = (sd(data[,i]) / mean(data[,i]))
-  sumCoef = sumCoef + tempCoef
+for (i in 3:length(inData[1,])){
+  tempCoef = (sd(inData[,i]) / mean(inData[,i]))
+  listCoef = rbind(listCoef, tempCoef)
 }
   
-sumCoef = sumCoef / dataLength
+# this is the baseline, that we can now use to filter through data
+baseCoef = mean(listCoef)
 
-# now we have the baseline, to filter the data set
-# now we run through the data set again, and drop uninteresting attributes
-for (j in 3:length(inData[,1])) {
-  currCoef = (sd(data[,i]) / mean())
+
+# filtering through data, keeping all attributes, that are above or equal to baseline coefficient
+throwAway <- c()
+for (i in 3: length(inData[1,])) {
+  tempCoef = (sd(inData[,i]) / mean(inData[,i]))
+  if (tempCoef < baseCoef) {
+    throwAway = rbind(throwAway, i) 
+  }
 }
+
+# excluding the attributes with too low interest
+inData = inData[, -throwAway] # losing 631, now have 716 attributes!
+
+
