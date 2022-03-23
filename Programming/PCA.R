15d567397fd24f9f1eb7fcd70d0b1699e42734b9
Troublemolle <jmoller72@gmail.com>
2022-03-23 13:37:46 +0100

    clean up filtering, make singleton ratio automatic
    Maybe use Michaelis-Menten fit to find knee point.

--- Programming/PCA.R
+++ Programming/PCA.R
@@ -1,6 +1,10 @@
 ### Principal component analysis ### 
-# Data needs to flipped..!
+# Data needs to transposed..!
 # Using 'spectral decomposition', examining covariances / correlation between variables
 # Using R-function 'princomp()'
 
+
+# first, transpose the matrix
+inData = outData
+inData = t(inData)
 
--- Programming/data_filtering.R
+++ Programming/data_filtering.R
@@ -2,39 +2,26 @@
 ### The goal of this function is to take in a file of multiple omics data ### 
 ### and then filter it by a given limit of coefficient ###
 
+# Loading libraries
+library(drc)
+library(ggplot2)
+
 options(warn = 0) # set=2 to end loops when warnings occur
 
 # Gonna write it out first, then make it into a function
-
-dataLength = len_16s + len_qpcr
 inData = data
 testID  <- inData$PIG_DATE
 OUA     <- inData$OUA
 
 inData = subset(inData, select = -c(PIG_DATE, OUA))
 
-## We start by running through and filtering out zero-columns
-throwAway <- c()
-#throwAway=seq(-1, NCOL(inData ))
-for (i in 1:NCOL(inData)) {
-  # if the variance is equal to zero, then its a zero-column
-  if (var(inData[,i]) == 0) {
-    throwAway = c(throwAway, i)
-    #throwAway[i]=i
-  }
-}
+# if the variance is equal to zero, then its a zero-column
+#which(unname(apply(inData, 2, var))==0)
+throwAway <- which(apply(inData, 2, var)==0)[]
 
 # dropping columns by index
 inData = inData[, -throwAway]
-
-
-# checking how many of the respective omics attributes were dropped
-#print("Number of metataxonomic attributes dropped") 
-#length(throwAway[throwAway <= len_16s]) # 12196
-
-#print("Number of genomic attributes dropped")
-#length(throwAway[throwAway > len_16s])  # 31
-
+rm(throwAway) # to save memory
 
 ## finding different interesting information on the set
 ## mean, var, STD, ratio of zeros
@@ -42,56 +29,35 @@ meanData  <- unname(apply(inData, 2, mean))
 stdData   <- unname(apply(inData, 2, sd))
 varData   <- unname(apply(inData, 2, var))
 ratioData <- unname(apply(inData, 2, function(x){length(which(x==0))/length(x)}))
+CV        <- unname(apply(inData, 2, function(x){sd(x)/mean(x)}))
                                       
 
-plot(meanData, main = "Mean Data")
-plot(stdData, main = "Standard Variations")
-plot(varData, main = "Variance")
-plot(ratioData, main = "Singleton ratios")
+plot(sort(meanData), main = "Mean Data")
+plot(sort(stdData), main = "Standard Variations")
+plot(sort(varData), main = "Variance")
+plot(sort(ratioData), main = "Singleton ratios")
+plot(sort(CV), main = "Relative standard deviation")
 
 
-# filtering out all of the singleton-zero ratios
-singletonValue = max(ratioData)
-# singletonValue = 0.85
+# fitting Michaelis-Menten function to data
+Vmax  <- max(ratioData)
+k     <- Vmax / 2
 
-throwAway <- c()
+mmModel <- nls(data = sort(ratioData), start = list(Vm=Vmax, K=k))
+#http://strata.uga.edu/8370/lecturenotes/nonlinearRegression.html
+"""
+par(mfrow=c(2,1), mar=c(2,2,0,0))
+plot(sort(ratioData))
+plot(diff(sort(ratioData))~sort(ratioData)[-1])
+abline(h=0.85)
 
-for (i in 1:NCOL(inData)) {
-  # if the value of ratioData is equal to singletonValue, throw away
-  if (ratioData[i] >= singletonValue) {
-    throwAway = c(throwAway, i)
-    #throwAway[i]=i
-  }
-}
-
-# dropping columns by index
-inData = inData[, -throwAway]
-
-## singleton-zero ratio filtering complete
-
-## filtering upon the relative standard deviation
-meanData  <- unname(apply(inData, 2, mean))
-stdData   <- unname(apply(inData, 2, sd))
-varData   <- unname(apply(inData, 2, var))
-ratioData <- unname(apply(inData, 2, function(x){length(which(x==0))/length(x)}))
-
-relativeSTD <- unname(apply(inData, 2, function(x){sd(x)/mean(x)}))
+par(mfrow=c(1,1), mar=c(2,2,2,2))
+"""
 
 
-relSTDValue = 0.85
-
-throwAway <- c()
-
-for (i in 1:NCOL(inData)) {
-  if (ratioData[i] >= relSTDValue) {
-    throwAway = c(throwAway, i)
-  }
-}
-
-# dropping columns by index
-inData = inData[, -throwAway]
 
 
+outData = inData
 
 
 
