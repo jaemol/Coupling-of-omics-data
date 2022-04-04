a606e8d7353282a2fbc53042a46f76e7d26c66f9
Troublemolle <Jmoller72@gmail.com>
2022-04-04 11:48:27 +0200

    Update data_filtering.R
    Correcting use of testID in script

--- Programming/data_filtering.R
+++ Programming/data_filtering.R
@@ -11,10 +11,10 @@ options(warn = 0) # set=2 to end loops when warnings occur
 data_filtering <- function(data) {
   # Gonna write it out first, then make it into a function
   inData = data
-  testID  <- inData$PIG_DATE
+  testID  <- inData$testID
   OUA     <- inData$OUA
   
-  inData = subset(inData, select = -c(PIG_DATE, OUA))
+  inData = subset(inData, select = -c(testID, OUA))
   
   # if the variance is equal to zero, then its a zero-column
   #which(unname(apply(inData, 2, var))==0)
