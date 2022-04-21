a6ddc119616e144c62853710b50a2f440b53d628
Troublemolle <jmoller72@gmail.com>
2022-04-21 12:40:55 +0200

    Update DBScan_clustering.R
    Corrected input data, found significant clusters and correlations!

--- Programming/DBScan_clustering.R
+++ Programming/DBScan_clustering.R
@@ -14,6 +14,9 @@ testID  <- inData$testID
 OUA     <- inData$OUA
 
 inData = subset(inData, select = -c(testID, OUA))
+#inData = t(inData)
+
+colnames(inData) <- testID
 
 # selecting arbitrary cluster number, just for visualization
 kmeans_model <- kmeans(inData, 3, nstart = 10)
@@ -34,14 +37,23 @@ findEpsi=function(L, minRange=0,maxRange=3, steps=0.1, maxY=200, minP=10) {
     points(i,length(unique(DB$cluster)), pch=16, col=1)
     points(i,length(which(DB$cluster==0)), pch=16,col=2)
     
-    print(i)
-    print(length(unique(DB$cluster)))
+    #print(i)
+    #sprintf("clusters: %f",length(unique(DB$cluster)))
     
   }
+  abline(v=seq(minRange,maxRange,steps))
+  
 }
 
-inDataScale=scale(inData, center = T, scale = T)
-findEpsi(inDataScale, minRange = 0, maxRange = 65, steps = 1 ,maxY=900)
+inDataScale=data.frame(scale(inData, center = T, scale = T))
+findEpsi(t(inDataScale), minRange = 30, maxRange = 32, steps = .1 ,maxY=100,minP = 3)
 
-dbscan::dbscan(inDataScale,6,65)
+DB=dbscan::dbscan(t(inDataScale),31.3,3)
 
+clusVars=data.frame(Clus=DB$cluster,Vars=colnames(inDataScale))
+
+subset(clusVars, Clus==0)
+
+
+plot(log1p(inData$DATA.Bacteria_Proteobacteria_Gammaproteobacteria_Enterobacterales_Enterobacteriaceae_Escherichia.Shigella),
+     log1p(inData$Increp64))
