b6d89cb46f729fb2089f05060786a1a666bc8b27
Troublemolle <Jmoller72@gmail.com>
2022-05-28 13:05:41 +0200

    Update network_analysis.R

--- Programming/network_analysis.R
+++ Programming/network_analysis.R
@@ -10,3 +10,10 @@ library(NetCoMi)
 devtools::install_github("stefpeschel/NetCoMi", dependencies = TRUE,
                          repos = c("https://cloud.r-project.org/",
                                    BiocManager::repositories()))
+
+
+# manual installation
+install.packages("stats") # containing pearson and spearman
+install.packages("WGCNA") # containing bicor
+install.packages("vegan") # containing vegdist
+install.packages("LaplaceDemon") # containing KLD (divergences)
