d2b47bfc9fb4dc4a1984a69b9261a86234827745
Troublemolle <jmoller72@gmail.com>
2022-05-30 08:46:53 +0200

    Update network_analysis.R
    NetCoMi install successfull!

--- Programming/network_analysis.R
+++ Programming/network_analysis.R
@@ -2,11 +2,26 @@
 ### Using Git-package NetCoMi ### 
 ### https://github.com/stefpeschel/NetCoMi ###
 
-# loading library
-library(NetCoMi)
+## installing package first time 
+"""
+if(!requireNamespace("BiocManager", quietly = TRUE)){
+  utils::install.packages("BiocManager")
+}
 
-#install.packages("devtools")
+BiocManager::install(pkgs = c("Biobase", "doSNOW", "fdrtool", "filematrix",
+                              "foreach", "graphics", "grDevices", "gtools",
+                              "huge", "igraph", "MASS", "Matrix", "phyloseq",
+                              "pulsar", "qgraph", "Rdpack", "snow", "SPRING",
+                              "stats", "utils", "vegan", "WGCNA"))
 
-devtools::install_github("stefpeschel/NetCoMi", dependencies = TRUE,
+devtools::install_github("GraceYoon/SPRING")
+devtools::install_github("zdk123/SpiecEasi")
+
+devtools::install_github("stefpeschel/NetCoMi", 
+                         dependencies = c("Depends", "Imports"),
                          repos = c("https://cloud.r-project.org/",
                                    BiocManager::repositories()))
+"""
+
+# loading NetCoMi library
+library(NetCoMi)
