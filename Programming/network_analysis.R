08eed3fedca094ef97bc767b1c1cd5ea8b0d070f
Troublemolle <jmoller72@gmail.com>
2022-05-30 10:03:55 +0200

    Update network_analysis.R
    trying to built network

--- Programming/network_analysis.R
+++ Programming/network_analysis.R
@@ -26,3 +26,36 @@ devtools::install_github("stefpeschel/NetCoMi",
 # loading NetCoMi library
 library(NetCoMi)
 
+# loading functions
+source("Programming/extracting_data_KAT.R")
+source("Programming/data_filtering.R")
+
+# loading data
+chosenWeek <- "Week 02"
+inData <- extracting_data_KAT(whichWeek = chosenWeek)
+
+# filtering data
+inData <- data_filtering(inData)
+
+# excluding testID and OUA (antibiotics used or not)
+#inData = data
+testID  <- inData$testID
+OUA     <- inData$OUA
+inData = subset(inData, select = -c(testID, OUA))
+
+
+# building single network with SPRING as association measure
+net_single <- netConstruct(amgut1.filt,
+                           filtTax = "highestFreq",
+                           filtTaxPar = list(highestFreq = 100),
+                           filtSamp = "totalReads",
+                           filtSampPar = list(totalReads = 1000),
+                           measure = "spring",
+                           measurePar = list(nlambda=10, 
+                                             rep.num=10),
+                           normMethod = "none", 
+                           zeroMethod = "none",
+                           sparsMethod = "none", 
+                           dissFunc = "signed",
+                           verbose = 3,
+                           seed = 123456)
