b8b105f71b86eb2366beae3ba64d172ea246e2b4
Troublemolle <jmoller72@gmail.com>
2022-06-15 11:03:59 +0200

    Update extracting_data_NATH.R
    loading in data, creating a data frame similar to KAT metataxdata

--- Programming/extracting_data_NATH.R
+++ Programming/extracting_data_NATH.R
@@ -2,5 +2,30 @@
 ### Metataxonomic and metabolomic data ###
 
 library(phyloseq)
+library(stringr)
 
 data_phys_original  <- readRDS("Data/allDataMetataxonomicNCLTEE.rds")
+
+# The actual data
+df.otu.metatax      <- as.data.frame(data_phys_original@otu_table)
+
+# The results / variables
+df.tax.metatax      <- as.data.frame(data_phys_original@tax_table)
+
+# The sample IDs
+sampleID.metatax    <- data_phys_original@sam_data$sample.name
+
+## setting up dataframe similar to Katrines
+df.full.metax <- as.data.frame(t(df.otu.metatax))
+rownames(df.full.metax) = sampleID.metatax
+
+# making string array with colnames
+colnames_array_metatax <- str_c("DATA.",str_replace_na(df.tax.metatax$domain, replacement="NA"),"_",
+                                str_replace_na(df.tax.metatax$phylum,  replacement="NA"),"_",
+                                str_replace_na(df.tax.metatax$class,   replacement="NA"),"_",
+                                str_replace_na(df.tax.metatax$order,   replacement="NA"),"_",
+                                str_replace_na(df.tax.metatax$family,  replacement="NA"),"_",
+                                str_replace_na(df.tax.metatax$genus,   replacement="NA"),".",
+                                str_replace_na(df.tax.metatax$species, replacement="NA"))
+                          
+colnames(df.full.metax) = colnames_array_metatax
