dd4c01daa871c19b48cb64e13f4ed8877df185ea
Troublemolle <jmoller72@gmail.com>
2022-07-15 13:43:14 +0200

    Update data_analyze.R

--- Programming/data_analyze.R
+++ Programming/data_analyze.R
@@ -11,14 +11,17 @@ data_analyze <- function(data, feature1, feature2, sampleRemovalOutliers="null")
   } 
   
   # finding the results and printing them out
-  paste("Generating results for features", feature1, "and", feature2)
+  paste("Generating results for features", feature1, "and", feature2,"\n")
   paste("The ordinal spearman correlation is:", cor(feat1, feat2, method = "spear"))
   paste("The log1p spearman correlation is:", cor(log1p(feat1), log1p(feat2), method = "spear"))
   paste("The ordinal pearson correlation is:", cor(feat1, feat2, method = "pear"))
   paste("The log1p pearson correlation is:", cor(log1p(feat1), log1p(feat2), method = "pear"))
   print("Plotting the correlation between the two features\n")
+  plot(unlist(feat1) ~ unlist(feat2), ylab = feature1, xlab = feature2, 
+       title(main = paste(feature1, "versus", feature2)))
   print("Building a linear model between the two features")
-  LM = lm(log1p(feat1)~log1p(feat2))
+  LM = lm(log1p(unlist(feat1))~log1p(unlist(feat2)))
+  LM = lm(unlist(feat1)~unlist(feat2))
   summary(LM)
   print("Plotting the related plots to the linear model")
   plot(LM)
