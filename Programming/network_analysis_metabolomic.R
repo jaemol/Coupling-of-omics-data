edf04fb848fa2e8d3d6cb2940000aa57016b3ded
Jacob Elskær Mollerup <44085709+jaemol@users.noreply.github.com>
2022-07-18 13:46:09 +0200

    Update network_analysis_metabolomic.R

--- Programming/network_analysis_metabolomic.R
+++ Programming/network_analysis_metabolomic.R
@@ -4,27 +4,27 @@
 
 ## installing package first time 
 
-# if(!requireNamespace("BiocManager", quietly = TRUE)){
-#   utils::install.packages("BiocManager")
-# }
-# 
-# BiocManager::install(pkgs = c("Biobase", "doSNOW", "fdrtool", "filematrix",
-#                               "foreach", "graphics", "grDevices", "gtools",
-#                               "huge", "igraph", "MASS", "Matrix", "phyloseq",
-#                               "pulsar", "qgraph", "Rdpack", "snow", "SPRING",
-#                               "stats", "utils", "vegan", "WGCNA"))
-# 
-# BiocManager::install("GO.db")
-# 
-# 
-# devtools::install_github("GraceYoon/SPRING")
-# devtools::install_github("zdk123/SpiecEasi")
-# 
-# devtools::install_github("stefpeschel/NetCoMi", 
-#                          dependencies = c("Depends", "Imports"),
-#                          repos = c("https://cloud.r-project.org/",
-#                                    BiocManager::repositories()))
-# 
+if(!requireNamespace("BiocManager", quietly = TRUE)){
+  utils::install.packages("BiocManager")
+}
+
+BiocManager::install(pkgs = c("Biobase", "doSNOW", "fdrtool", "filematrix",
+                              "foreach", "graphics", "grDevices", "gtools",
+                              "huge", "igraph", "MASS", "Matrix", "phyloseq",
+                              "pulsar", "qgraph", "Rdpack", "snow", "SPRING",
+                              "stats", "utils", "vegan", "WGCNA"))
+
+BiocManager::install("GO.db")
+
+# install.packages("devtools")
+devtools::install_github("GraceYoon/SPRING")
+devtools::install_github("zdk123/SpiecEasi")
+
+devtools::install_github("stefpeschel/NetCoMi",
+                         dependencies = c("Depends", "Imports"),
+                         repos = c("https://cloud.r-project.org/",
+                                   BiocManager::repositories()))
+
 
 # loading NetCoMi library
 library(NetCoMi)
