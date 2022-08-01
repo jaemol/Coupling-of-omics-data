9c192fd349b4cf29e68541d54da63f9a928bfc0e
Jacob Elskær Mollerup <44085709+jaemol@users.noreply.github.com>
2022-08-01 14:07:25 +0200

    Update evalutation_of_normalization.R

--- Programming/evalutation_of_normalization.R
+++ Programming/evalutation_of_normalization.R
@@ -25,10 +25,18 @@ source("Programming/Functions.R")
 # and so, this need to be incorporated via 'STRATA' in the PerMANOVA
 
 # getting the data
-list[data_peak, strata_field]   <- getMetabDataNormEval(cutOffMetabMass = 200, whichNormalization = "peak")
-list[data_median, strata_field] <- getMetabDataNormEval(cutOffMetabMass = 200, whichNormalization = "median")
-list[data_mad, strata_field]    <- getMetabDataNormEval(cutOffMetabMass = 200, whichNormalization = "mad")
-list[data_mean, strata_field]    <- getMetabDataNormEval(cutOffMetabMass = 200, whichNormalization = "mean")
+list[data_metab, strata_field]  <- getMetabDataNormEval(cutOffMetabMass = 200)
+
+# implementing the different normalizations
+data_peak   <- as.data.frame(apply(data_metab,MARGIN = 2, function(x){x/max(x)})); rownames(data_peak)=rownames(data_metab); colnames(data_peak)=colnames(data_metab)
+data_median <- as.data.frame(apply(data_metab,MARGIN = 2, function(x){x/(median(x)+1)})); rownames(data_peak)=rownames(data_metab); colnames(data_peak)=colnames(data_metab)
+data_mean   <- as.data.frame(apply(data_metab,MARGIN = 2, function(x){x/(mean(x)+1)})); rownames(data_peak)=rownames(data_metab); colnames(data_peak)=colnames(data_metab)
+data_mad    <- as.data.frame(apply(data_metab,MARGIN = 2, function(x){x/(mad(x, center = median(x), na.rm = FALSE, constant = 1)+1)})); rownames(data_peak)=rownames(data_metab); colnames(data_peak)=colnames(data_metab)
+
+# list[data_peak, strata_field]   <- getMetabDataNormEval(cutOffMetabMass = 200, whichNormalization = "peak")
+# list[data_median, strata_field] <- getMetabDataNormEval(cutOffMetabMass = 200, whichNormalization = "median")
+# list[data_mad, strata_field]    <- getMetabDataNormEval(cutOffMetabMass = 200, whichNormalization = "mad")
+# list[data_mean, strata_field]    <- getMetabDataNormEval(cutOffMetabMass = 200, whichNormalization = "mean")
 
 dist_median <- as.matrix(dist(data_median, method = "euclidean"))
 here <- as.dist(dist_median)
@@ -39,4 +47,10 @@ adonis2(data_median ~ strata_field, strata = strata_field, permutations = 999, b
 adonis2(data_mad ~ strata_field, strata = strata_field, permutations = 999, by = NULL, method = "euclidean")
 #adonis2(data_mad ~ strata_field, strata = NULL, permutations = 999, by = NULL)
 adonis2(data_mean ~ strata_field, strata = strata_field, permutations = 999, by = NULL, method = "euclidean")
+
+
+
+
+
+
 
