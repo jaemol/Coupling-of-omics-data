a8a7f578441d4d9c30ede12055698d4ef4ce53a7
Jacob Elskær Mollerup <44085709+jaemol@users.noreply.github.com>
2022-08-06 16:54:06 +0200

    Update network_analysis_metabolomic.R

--- Programming/network_analysis_metabolomic.R
+++ Programming/network_analysis_metabolomic.R
@@ -2,30 +2,6 @@
 ### Using Git-package NetCoMi ### 
 ### https://github.com/stefpeschel/NetCoMi ###
 
-## installing package first time 
-
-if(!requireNamespace("BiocManager", quietly = TRUE)){
-  utils::install.packages("BiocManager")
-}
-
-BiocManager::install(pkgs = c("Biobase", "doSNOW", "fdrtool", "filematrix",
-                              "foreach", "graphics", "grDevices", "gtools",
-                              "huge", "igraph", "MASS", "Matrix", "phyloseq",
-                              "pulsar", "qgraph", "Rdpack", "snow", "SPRING",
-                              "stats", "utils", "vegan", "WGCNA"))
-
-BiocManager::install("GO.db")
-
-# install.packages("devtools")
-devtools::install_github("GraceYoon/SPRING")
-devtools::install_github("zdk123/SpiecEasi")
-
-devtools::install_github("stefpeschel/NetCoMi",
-                         dependencies = c("Depends", "Imports"),
-                         repos = c("https://cloud.r-project.org/",
-                                   BiocManager::repositories()))
-
-
 # loading NetCoMi library
 library(NetCoMi)
 
