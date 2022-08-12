c141bc30d76b69e6bfc1e8da4a0ef23327f58239
Jacob Elskær Mollerup <44085709+jaemol@users.noreply.github.com>
2022-08-12 19:56:54 +0200

    Update extracting_data_NCLTEE.R

--- Programming/extracting_data_NCLTEE.R
+++ Programming/extracting_data_NCLTEE.R
@@ -7,7 +7,7 @@ library(stringr)
 library(gsubfn)
 
 # beginning function
-extracting_data_NATH <- function(whichWeek="null", whichTaxLevel="species", 
+extracting_data_NCLTEE <- function(whichWeek="null", whichTaxLevel="species", 
                                  cutOffMetabMass=200, whichNormalization) {
   # loading data
   #data_phys_original  <- readRDS("Data/allDataMetataxonomicNCLTEE.rds")
@@ -112,7 +112,7 @@ extracting_data_NATH <- function(whichWeek="null", whichTaxLevel="species",
       
     #if (length(num_grep) == 1 && num_grep > 400) { 
     # find which sample is talked about
-    metaDataRow = which(metadata_metabolomics$ï..Sample.no...MCCe. == num_grep)
+    metaDataRow = which(metadata_metabolomics$Sample.no...MCCe. == num_grep)
     
     # ensuring no medium control (blank samples)
     #if (!(num_grep==449||num_grep==458)){
